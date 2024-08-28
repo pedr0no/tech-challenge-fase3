8b6176b9ba47c92a66106176d263916486bbaa71
Pedro Ono <pedroono@icloud.com>
2024-08-28 20:09:18 -0300

    Criando projeto Spring

---
+++ tablereservation/src/main/java/br/com/fiap/techchallenge/tablereservation/TablereservationApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge.tablereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablereservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablereservationApplication.class, args);
+	}
+
+}
